45c1d6ac11a51d5aa6ef3c67149fc0face63beba
Guilhem AZZANO <g.azzano74@gmail.com>
2019-06-06 22:03:49 +0100

    Rewriting everything in object

--- Architecture/robot.py
+++ Architecture/robot.py
@@ -1,23 +1,27 @@
 
 import numpy as np
 import math
-from picamera import PiCamera
-import beacon as Beacon
-class robot:
-    def __init__(self,x,y,a):
-        self.x = int(round(x/10))
-        self.y = int(round(y/10))
-        self.angle = math.radians(a)
-        self.ir_range = 30
-        self.ir_range_back = 43
-        self.cam = PiCamera()
-        self.cam.resolution = (1920,1080)
-        self.cam.shutter_speed = 4000
-        self.size_front = 33
-        self.size_back = 7
-        self.size_side = 20
-        self.diag_front = math.sqrt(self.size_front* self.size_front + self.size_side*self.size_side)
-        self.sensor_List = []
+import  time, sys , serial
+import threading
+from collections import deque
+from Serial_ import *
+from parse import *
+
+SERIAL_PORT_ROBOT = '/dev/ttyACM0'
+SERIAL_PORT_COllECTOR = '/dev/ttyACM1'
+SERIAL_RATE = 115200
+class Robot_Class:
+    def __init__(self,x=0,y=0,a=0):
+        self.X_Pos = x
+        self.Y_Pos = y
+        self.Angle_Deg = a
+        self.Robot_Speed=750
+        self.map= Map()
+        self.Goal_X=x
+        self.Goal_Y=y
+        self.Goal_Temp_X=x
+        self.Goal_Temp_Y=y
+        self.Goal_Temp=False
         self.ir_sensors_state = {
             'a':0,
             'b':0,
@@ -32,123 +36,131 @@ class robot:
             'k':0,
             'l':0,
             'm':0}
-        self.ir_sensors_position = {
-                            #BACK
-                           'a':[84/10,133.4,160,self.ir_range_back],
-                           'b':[171/10,107,-135,self.ir_range_back],
-                           'c':[171/10,-107,135,self.ir_range_back],
-                           'd':[84/10,-133.4,-160,self.ir_range_back],
+        self.buf_broadcast = deque([]) # Queue of message for broadcasting via TCP IP
+        self.buf_stm = deque([])
+        self.ser = serial.Serial(SERIAL_PORT_ROBOT, SERIAL_RATE) # Connection to STM32 ROBOT
+        self.ser_Collector= serial.Serial(SERIAL_PORT_COllECTOR,115200)
+        self.serial_T = serialTread(self.ser)
+        threading.Thread(target=self.serial_T.run, args=(self.buf_broadcast,self.buf_stm)).start()#create serial thread for robot control
+    def Send_Messages(self,All_Commands):
+        for string_ in All_Commands:
+            self.buf_stm.append(string_)#Send every messages
+    def Get_Read_Buffer(self):
+        return self.buf_broadcast
+    def Collect_Message(self):
+        if(len(self.buf_broadcast)):
+            return self.buf_broadcast.popleft()
+        return None
+    def Wait_For_A_Message(self, Timeout):#Timeout in seconds
+        Start_Time=time.time()
+        while(True):
+            Message=self.Collect_Message()
+            if((time.time()-Start_Time)>Timeout and Message==None):
+                return None
+            elif(Message !=None):
+                return Message
+    def Wait_For_M3_Answer(self, Timeout):
+        Start_Time=time.time()
+        while(True):
+            Message=self.Collect_Message()
+            if((time.time()-Start_Time)>Timeout and Message==None):
+                return None, None
+            elif(Message is not None):
+                Valid, Output= self.Is_it_M3_Answer(Message)
+                print(Message)
+                if(Valid is not False):
+                    return Valid, Output
+    def Is_it_M3_Answer(self, Message):
+        if(Message is None):
+            return False, None
+        str2=parse("M0 X{} Y{} A{} T{} S{}",Message)
+        if(str2 is not None):
+            array_argument=np.zeros(5)
+            for i in range(5):
+                array_argument[i]=float(str2[i])
+            self.Set_Robot_Position(array_argument[0],array_argument[1],array_argument[2])
+            feedback_sensors = int(array_argument[4])# feedback sensor MLKJIHGFEDCBA 
+            self.Set_Sensor_State(feedback_sensors)
+            if array_argument[3]==0: #T0 argument Arrived or sensor
+                return True, 'Arrived'
+            elif array_argument[3]==1:
+                return True, 'Timeout'
+            elif array_argument[3]==2:
+                return True, 'Blocked'
             
-            
-                           'e':[287/10,-41,-15,self.ir_range_back],
-                           'f':[186/10,-95.8,-20,self.ir_range],
-                           'g':[287/10,41,15,self.ir_range_back],
-                           'h':[186/10,95.8,20,self.ir_range],
-                           'i':[369/10,-31.91,-130,self.ir_range],
-                           'j':[369/10,31.91,130,self.ir_range],
-                           'k':[333/10,7,55,self.ir_range],
-                           'l':[333/10,-7,-55,self.ir_range],
-                           'm':[340/10,0,0,self.ir_range],}
+        return False, None
+    def Go_To(self,X,Y,R,level=0):
+        print('Going to', str(X), ' ', str(Y), 'with R', str(R), 'level', str(level))
+        X_Robot , Y_Robot, A_Robot = self.Get_Robot_Position()
+        Timeout = (math.sqrt((X-X_Robot)*(X-X_Robot)+(Y-Y_Robot)*(Y-Y_Robot))/self.Robot_Speed)*1000
+        Set_Coordinate=[X,Y,self.Robot_Speed,max(Timeout*1.2,1000),R]
+        while(True):
+            self.Send_Go_To_Coordinate(Set_Coordinate)
+            M0_State, Output=self.Wait_For_M3_Answer((Timeout*1.2)/1000+400)
+            if(M0_State== False):
+                print('STM32 Lack of answer')
+            else:
+                print(Output)
+                if(Output=='Blocked'):
+                    X_,Y_,R_=self.Obstacle_Avoidance_Calculation()
+                    self.Go_To(X_,Y_,R_,level+1)
+                    pass
+                elif(Output=='Timeout'):
+                    break;
+                elif(Output=='Arrived'):
+                    break;
+                    
+
+        print('End of', level)
+    def Send_Go_To_Coordinate(self,Set_Coordinate):
+        X_Des=Set_Coordinate[0]
+        Y_Des=Set_Coordinate[1]
+        Max_Speed=Set_Coordinate[2]
+        TimeOut=Set_Coordinate[3]
+        Backward_Parameter=Set_Coordinate[4]
+        """Create messages"""
+        G0_String='G0 X' + str(round(X_Des))+' Y'+str(round(Y_Des))+' T'+str(round(TimeOut))+ ' R'+str(round(Backward_Parameter))+'\r\n'
+        M201_String='M201 H0 S' + str(Max_Speed)+'\r\n'
+        M3_String='M3 H3\r\n'
+        All_Commands = (G0_String,M201_String,M3_String)
+        self.Send_Messages(All_Commands)
         
-                           
-        self.ir_sensors = {
-            'a':[0,0  ],
-            'b':[0,0  ],
-            'c':[0,0  ],
-            'd':[0,0  ],
-            'e':[0,0  ],
-            'f':[0,0  ],
-            'g':[0,0  ],
-            'h':[0,0  ],
-            'i':[0,0  ],
-            'j':[0,0  ],
-            'k':[0,0  ],
-            'l':[0,0  ],
-            'm':[0,0  ],}
-        
-        for sensor in self.ir_sensors:
-            self.set_sensor_position(sensor,self.ir_sensors_position[sensor][0],self.ir_sensors_position[sensor][1],self.ir_sensors_position[sensor][2] )
-            
+    def Set_Robot_Position(self,x,y,a):
+        self.X_Pos = x
+        self.Y_Pos = y
+        self.Angle_Deg = a
+        command = 'G92 X' + str(round(self.X_Pos)) +' Y'+str(round(self.Y_Pos))+ ' A'+str(round(self.Angle_Deg))+ '\r\n'
+        self.Send_Messages(command)
+    def Set_Robot_Goal(self,Coord):
+        self.Goal_X = x
+        self.Goal_Y = y
+        self.Goal_Temp = False
+    def Set_Robot_Temp_Goal(self,Coord):
+        self.Goal_Temp_X = x
+        self.Goal_Temp_Y = y
+        self.Goal_Temp = True
     
+    def Realize_Your_Dream(self):
+        if(self.Goal_Temp):
+            pass
+    def Get_Robot_Position(self):
+        return self.X_Pos,self.Y_Pos,self.Angle_Deg
+
+    def Get_Beacon_Position(self):
+        return self.X_Pos,self.Y_Pos,self.Angle_Deg
     
-    
-    def set_sensor_position(self, sensor_id, dist,theta,angle_sensor ):
-        #dist = math.sqrt(x*x+y*y)
-        theta = math.radians(theta)
-        angle_sensor = math.radians(angle_sensor)
-        #ref_vector = (math.cos(self.angle),math.sin(self.angle))
-        #v2 = (x,y)
-        #theta = self.anglevec(ref_vector, v2)
-        #if sensor_id in ['c','d','f','e','k','i']:
-         #   theta = - theta
-        #theta = math.atan2(ref_vector[1],ref_vector[0]) - math.atan2(y,x)
-        
-       
-        self.ir_sensors[sensor_id] = [round(dist*math.cos(self.angle + theta) + (self.ir_sensors_position[sensor_id][3] * math.cos(self.angle +angle_sensor))),
-                                     round(dist*math.sin(self.angle + theta) + (self.ir_sensors_position[sensor_id][3]  *math.sin(self.angle +angle_sensor))),]
-
-        
-          
-    
-    def set_position(self, x,y,a):
-        self.x = int(round(x/10))
-        self.y = int(round(y/10))
-        self.angle = math.radians(a)
-        for sensor in self.ir_sensors:
-            self.set_sensor_position(sensor,self.ir_sensors_position[sensor][0],self.ir_sensors_position[sensor][1],self.ir_sensors_position[sensor][2]  )
-        
- 
-        
-    def get_position(self):
-        return [self.x*10,self.y*10,math.degrees(self.angle)]
-
-    def dotproduct(self,v1, v2):
-        return sum((a*b) for a, b in zip(v1, v2))
-
-    def length(self,v):
-        return math.sqrt(self.dotproduct(v, v))
-
-    def anglevec(self,v1, v2):
-        return math.acos(self.dotproduct(v1, v2) / (self.length(v1) * self.length(v2)))
-
-
-    def get_beacon_position(self):
-        Beacon_pos = Beacon.beacon_main(self.cam)
-        Robot_Pos=self.get_position()
-        if len(Beacon_pos)>0:
-            if(Robot_Pos[0]<3000 and Robot_Pos[1]<3000):
-                return Robot_Pos[0],Robot_Pos[1],Beacon_pos[2]
-            return Beacon_pos
-        else:
-            print('Beacon not working');
-            return Robot_Pos[0],Robot_Pos[1],Robot_Pos[2]
-         
-
-    
-
-    def set_sensor_state(self,state):
+    def Set_Sensor_State(self,state):
         obstacle_position = []
         sensors_state = {'a':1,'b':2,'c':4,'d':8,'e':16,'f':32,'g':64,'h':128,'i':256,'j':512,'k':1024,'l':2048,'m':4096}
-
         mask = 1 
         for sensor in  sensors_state:
             if state&(mask*sensors_state[sensor])>0:
                 sensors_state[sensor]=1
-
             else:
                 sensors_state[sensor]=0
         self.ir_sensors_state=sensors_state
         print(self.ir_sensors_state)
-
-        for i in sensors_state :
-            if sensors_state[i] :
-
-                xi = self.ir_sensors[i][0] + self.x
-                yi = self.ir_sensors[i][1] + self.y
-                #obstacle_position.append([xi,yi])
-                
-        return obstacle_position
-    def get_sensor_state(self):
+    def Get_Sensor_State(self):
         return self.ir_sensors_state
                     
     def Has_reached_final_position(self, Final_Pos):
@@ -159,23 +171,117 @@ class robot:
         if(math.sqrt(Distance_X*Distance_X+Distance_Y*Distance_Y)<200):
             return True
         return False
-    def Free_Space_Around(self,Sensor_State, Map_State):
+    def Get_Free_Space_Around(self,Sensor_State):
             
         #Get information from sensor and map
-        print(Map_State)
         Space_Free = {'Front':1,'Left':1,'Right':1,'Back':1 ,'FrontLeft':1,'FrontRight':1, 'BackLeft':1, 'BackRight':1, }
-        if(Sensor_State['k'] or Sensor_State['l'] or Sensor_State['m'] or Map_State["Front"] ):
+        if(Sensor_State['k'] or Sensor_State['l'] or Sensor_State['m']):
             Space_Free['Front']=0;
-        if(Sensor_State['a'] or Sensor_State['b'] or Sensor_State['c'] or Sensor_State['d'] or Map_State["Back"] ):
+        if(Sensor_State['a'] or Sensor_State['b'] or Sensor_State['c'] or Sensor_State['d']):
             Space_Free['Back']=0;
-        if(Sensor_State['h'] or Sensor_State['g'] or Sensor_State['j'] or  Map_State["Left"]):
+        if(Sensor_State['h'] or Sensor_State['g'] or Sensor_State['j']):
             Space_Free['Left']=0;
-        if(Sensor_State['e'] or Sensor_State['f'] or Sensor_State['i'] or  Map_State["Right"]):
+        if(Sensor_State['e'] or Sensor_State['f'] or Sensor_State['i']):
             Space_Free['Right']=0;
-           
         return Space_Free
+    
+    def Can_I_Go(self, Objectif):
+        X_Robot,Y_Robot,A_Robot = self.Get_Robot_Position()
+        Objectif_X, Objectif_Y = Objectif
+        if(Objectif_X<0 or Objectif_X>8000 or Objectif_Y<0 or Objectif_Y>8000 ):
+            return False
+        elif(self.map.IsRockZone(Objectif) or self.map.IsPlatformZone(Objectif)):#Rock zone
+            return False
+        else:
+            return True
+    def Which_Zone_Am_I(self):
+        X_Robot,Y_Robot,A_Robot = self.Get_Robot_Position()
+        Zone = {'Normal':0,'Rock':0,'Platform':0,'Grass':0 }
+        if(self.map.IsRockZone((X_Robot,Y_Robot))):
+            Zone['Rock']=1
+        elif(self.map.IsPlatformZone((X_Robot,Y_Robot))):
+            Zone['Platform']=1
+        elif(self.map.IsGrassZone((X_Robot,Y_Robot))):
+            Zone['Grass']=1
+        elif(self.map.IsNormalZone((X_Robot,Y_Robot))):
+            Zone['Normal']=1  
+        return Zone   
+    def Obstacle_Avoidance_Calculation(self):
+        return self.map.Calculate_Side_Checkpoint(self.Get_Robot_Position(),self.Get_Free_Space_Around(self.Get_Sensor_State()))
+    def Start_Collector():
+        self.ser_Collector.write(b'G1 R1500 L2100\r\n')
+    def Reverse_Collector():
+        self.ser_Collector.write(b'G1 R-1500 L-2100\r\n')
+    def Stop_Collector():
+        self.ser_Collector.write(b'G1 R0 L0\r\n')
 
 
+class Map:
+    def IsRockZone(self,Objectif):
+        Objectif_X, Objectif_Y = Objectif
+        if(Objectif_X<3000 and Objectif_Y>5000):
+            return True
+        return False
+    def IsPlatformZone(self,Objectif):
+        Objectif_X, Objectif_Y = Objectif
+        if(Objectif_X>5000 and Objectif_Y>6000):
+            return True
+        return False
+    def IsGrassZone(self,Objectif):
+        Objectif_X, Objectif_Y = Objectif
+        if(Objectif_X>4000 and Objectif_Y<2000):
+            return True
+        return False
+    def IsNormalZone(self,Objectif):
+        return not(self.IsRockZone(Objectif) or self.IsPlatformZone(Objectif) or self.IsGrassZone(Objectif))
+    def IsAccessible(self,Objectif):
+        return (self.IsNormalZone(Objectif) or self.IsGrassZone(Objectif))
+    def Calculate_Side_Checkpoint(self,Robot_Pos, Space_Around):
+        X, Y, A = Robot_Pos
+        X_Robot, Y_Robot, A_Robot= Robot_Pos
+        if(not(Space_Around['Front']) and Space_Around['Right']):
+            X_New=X+math.cos(math.radians(A-90))*300
+            Y_New=Y+math.sin(math.radians(A-90))*300
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Front go to Right',X_New, Y_New)
+                return X_New,Y_New,0
+        if(not(Space_Around['Front']) and Space_Around['Left']): 
+            X_New=X+math.cos(math.radians(A+90))*300
+            Y_New=Y+math.sin(math.radians(A+90))*300
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Front go to left',X_New, Y_New)
+                return X_New,Y_New,0
+        if(Space_Around['Front'] and( not(Space_Around['Right'])or not(Space_Around['Left']))):
+            X_New=X+math.cos(math.radians(A))*300
+            Y_New=Y+math.sin(math.radians(A))*300
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Avoidance Side go to the Front',X_New, Y_New)
+                return X_New,Y_New,0
+        if(not(Space_Around['Front']) and not(Space_Around['Left']) and not(Space_Around['Right']) ):
+            #Back LEFT
+            X_New=X+math.cos(math.radians(A+120))*450
+            Y_New=Y+math.sin(math.radians(A+120))*450
+            #Back Right
+            
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Avoidance Back right',X_New, Y_New)
+                return X_New,Y_New,1
+            X_New=X+math.cos(math.radians(A-120))*450
+            Y_New=Y+math.sin(math.radians(A-120))*450
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Avoidance Back Left',X_New, Y_New)
+                return X_New,Y_New,1
+            X_New=X+math.cos(math.radians(A+180))*300
+            Y_New=Y+math.sin(math.radians(A+180))*300
+            if(self.IsAccessible((X_New,Y_New))):
+                print('Avoidance Back',X_New, X_New)
+                return X_New,Y_New,1
+        
+        X_New=X
+        Y_New=Y
+        print('Avoidance not found',X_New, Y_New)
+        return X_New,Y_New,0
 
+        
 
 
